b65e6383c604b7493353037b521f95b1042c158a
./cosmic <91699202+dotslashCosmic@users.noreply.github.com>
2025-10-25 02:46:52 -0400

    Added heap allocation

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -3,6 +3,8 @@
 
 //! CosmOS Kernel Library
 
+extern crate alloc;
+
 pub mod arch;
 pub mod mm;
 pub mod serial;
@@ -13,4 +15,14 @@ pub fn hlt_loop() -> ! {
     loop {
         x86_64::instructions::hlt();
     }
+}
+
+/// Allocate memory on the heap, execute a closure, then deallocate
+#[inline]
+pub fn with_heap_alloc<T, F, R>(initial_value: T, f: F) -> R
+where
+    F: FnOnce(&mut T) -> R
+{
+    let mut boxed = alloc::boxed::Box::new(initial_value);
+    f(&mut *boxed)
 }
